f169e63ff6c7b84cd55b9cb1d6d6f0ba9fcff75a
michaeldtodd <mtodd@cloverhound.com>
2015-10-28 14:09:31 -0400

    changed to pm25

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -5,16 +5,16 @@ def index
 
 response = RestClient::Request.execute(
    :method => :get,
-   :url => 'https://api.xively.com/v2/feeds/819176661/datastreams/PM10count.json',
+   :url => 'https://api.xively.com/v2/feeds/819176661/datastreams/PM25count.json',
    :headers => {"X-ApiKey" => "yGnWbZJJWfKB5sIImNO7xRXRA6KFUlUJH1hYWsPs065Q6fYX"}
 )
 
 responsejson = JSON.parse(response)
 
-pm10count = responsejson['current_value']
+pm25count = responsejson['current_value']
 
 t = Tropo::Generator.new
-t.say(:value => "The current PM10 count is" + pm10count + "particles per cubic foot")
+t.say(:value => "The current PM 2 point 5 count is" + pm25count + "particles per cubic foot")
 render :json => t.response
 end
 
